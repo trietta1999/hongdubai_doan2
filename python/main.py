9ee3da3b1fae25b88b6fed574c04caab7fe4a0c8
Triet Ta <tadongthuyuyen@gmail.com>
2022-11-08 21:38:09 +0700

    firebase server

---
+++ python/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time, random
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication("https://do-an-2-ute-default-rtdb.firebaseio.com/" None)
+data = firebase.get('BED_ROOM/data', None).split('-')
+path = ["BED_ROOM/nhiet_do", "BED_ROOM/do_am", "BED_ROOM/khi_ga", "BED_ROOM/cb_as", "BED_ROOM/ti_vi", "BED_ROOM/may_lanh", "BED_ROOM/den"]
+
+for i in range(len(path)):
+	firebase.put("/", path[i], data[i + 1])
+
+time.sleep(0.5)
+st.experimental_rerun()
