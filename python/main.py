3b7c3087ebe7fdaafff31628dc1565ea1c7827cb
Triet Ta <tadongthuyuyen@gmail.com>
2022-11-08 21:40:29 +0700

    update

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time, random
 from firebase import firebase
 
-firebase = firebase.FirebaseApplication("https://do-an-2-ute-default-rtdb.firebaseio.com/" None)
+firebase = firebase.FirebaseApplication("https://do-an-2-ute-default-rtdb.firebaseio.com/", None)
 data = firebase.get('BED_ROOM/data', None).split('-')
 path = ["BED_ROOM/nhiet_do", "BED_ROOM/do_am", "BED_ROOM/khi_ga", "BED_ROOM/cb_as", "BED_ROOM/ti_vi", "BED_ROOM/may_lanh", "BED_ROOM/den"]
 
