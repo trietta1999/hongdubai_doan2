5f3c9c730b6bfdda965001013d189513f42918b9
Triet Ta <tadongthuyuyen@gmail.com>
2022-11-11 22:22:34 +0700

    update

--- python/main.py
+++ python/main.py
@@ -7,7 +7,7 @@ data = firebase.get('BED_ROOM/data', None).split('-')
 path = ["BED_ROOM/nhiet_do", "BED_ROOM/do_am", "BED_ROOM/khi_ga", "BED_ROOM/cb_as", "BED_ROOM/ti_vi", "BED_ROOM/may_lanh", "BED_ROOM/den"]
 
 for i in range(len(path)):
-	firebase.put("/", path[i], data[i + 1])
+	firebase.put("/", path[i], int(data[i + 1]))
 
 time.sleep(2)
 st.experimental_rerun()
